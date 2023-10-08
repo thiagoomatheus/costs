be6680ce84dbc1394b37c2a640f1d13c45b1b2c0
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-10-08 16:12:58 -0300

    feat: menu hamburguer

--- costs/src/components/header/Navbar.tsx
+++ costs/src/components/header/Navbar.tsx
@@ -1,11 +1,12 @@
 type NavbarProps = {
     children?: React.ReactNode
+    hidden: boolean
 }
 
-export default function Navbar( {children}: NavbarProps ) {
+export default function Navbar( {children, hidden}: NavbarProps ) {
     return (
         <>
-            <ul className="flex flex-row justify-between gap-x-2 items-center list-none sm:gap-x-5 text-white">
+            <ul className={`${hidden ? 'flex' : 'hidden'} absolute bg-black top-[30px] rounded-md px-4 pb-3 right-0 flex-col sm:flex sm:flex-row justify-between gap-2 items-center list-none sm:gap-x-5 text-white`}>
                 {children}
             </ul>
         </>
